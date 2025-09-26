a9f98af8bf06bce3b1a51264d5538ec77068a19c
ChaiyapatOam <chaiyapatoam12@gmail.com>
2025-09-27 00:53:40 +0700

    feat: en&th dict for activity(workshop)

---
+++ src/i18n/dict/workshop/th.ts
@@ -0,0 +1,7 @@
+export default {
+  workshop: {
+    title: "Workshops",
+    description: "Explore our workshops",
+    seeAll: "See all workshops",
+  },
+};
--- src/i18n/getDict.ts
+++ src/i18n/getDict.ts
@@ -1,4 +1,10 @@
 import th from "./dict/th";
 import en from "./dict/en";
+import workshopTH from "./dict/workshop/th";
+import workshopEN from "./dict/workshop/en";
 
 export const getDict = (lang: "th" | "en") => (lang === "th" ? th : en);
+export const getWorkshopDict = (lang: "th" | "en") => (lang === "th" ? workshopTH : workshopEN);
+
+export type TDict = ReturnType<typeof getDict>;
+export type TWorkshopDict = ReturnType<typeof getWorkshopDict>;
